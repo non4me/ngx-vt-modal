45b4a520d92374f64fb33ba2e2839afbceef168b
Vladimir Troyanenko <post@itman.ru>
2022-07-30 22:31:47 +0200

    0.0.3

---
+++ projects/ngx-vt-modal-lib/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of store-modal
+ */
+
+export * from './lib/ngx-vt-modal.service';
+export * from './lib/ngx-vt-modal.module';
+export * from './lib/model/ngx-vt-modal-size';
+export * from './lib/model/ngx-vt-modal-options';
+export * from './lib/model/ngx-vt-modal-close-status';
